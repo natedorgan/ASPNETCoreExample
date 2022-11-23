0d7a75232f760ebe4c8be0eccc6adc46b8fb34c0
natedorgan <natedorgandevops@gmail.com>
2022-11-23 12:47:07 -0800

    Update site.js

--- ASPNETCoreExample/wwwroot/js/site.js
+++ ASPNETCoreExample/wwwroot/js/site.js
@@ -10,3 +10,30 @@ function newColor() {
 function oldColor() {
     document.getElementById("header").style.color = "maroon";
 }
+
+function moveBox() {
+  
+var speed = 6,
+    
+    moveBox = function(moveBy) {
+      var el = document.getElementById('box'),
+          left = el.offsetLeft;
+      
+      if ( (moveBy > 0 && left > 500) || (moveBy < 0 && left < 51) ) {
+        clearTimeout(timer);
+        timer = setInterval(function() {
+          moveBox(moveBy * -1);  // to change direction
+        }, speed);
+      }
+      
+      el.style.left = left + moveBy + 'px';
+
+    };
+  
+var timer = setInterval(
+  function() { 
+    moveBox(5); 
+  }
+  , speed);
+  
+}
